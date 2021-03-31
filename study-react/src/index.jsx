8ee853f471b36ed55f9a5edc4b31cb40f980420d
Arthur-art <arthurteixeira.guts@gmail.com>
2021-03-31 01:28:20 -0300

    redatorando indexjsx

--- study-react/src/index.jsx
+++ study-react/src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import This from './components/This'
+import IfElse from './components/IfElse'
 
 ReactDOM.render(
     <React.StrictMode>
         <This/>
+        <IfElse/>
     </React.StrictMode>,
     document.getElementById('container')
 );
