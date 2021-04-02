0bffd5a367d2eee0151862f636c4b74b1f060279
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-02 14:34:32 -0300

    setState - clock new date isPrecise refatorado

--- study-react/src/components/StateDidUpdate.jsx
+++ study-react/src/components/StateDidUpdate.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React from "react";
 
 export default class StateDidUpdate extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { date: new Date(), isPrecise:true, };
-    this.setStateClock = this.setStateClock.bind(this)
+    this.state = { date: new Date(), isPrecise: true, clock: true };
+    this.setStateClock = this.setStateClock.bind(this);
+    this.setStateClockInit = this.setStateClockInit.bind(this);
   }
-  setStateClock(){
-      const newIsPrecise = this.state.isPrecise === true ? false : true;
-      this.setState({
-          isPrecise: newIsPrecise
-      })
+  setStateClock() {
+    const newIsPrecise = this.state.isPrecise === true ? false : true;
+
+    this.setState({
+      isPrecise: newIsPrecise,
+    });
+  }
+  setStateClockInit() {
+    const clockInit = this.state.clock === true ? false : true;
+    this.setState({
+      clock: clockInit,
+    });
   }
   render() {
+    const Show = this.state.clock === true ? "Hidden" : "Show";
+    const ClockPrecise = this.state.isPrecise
+      ? this.state.date.toISOString()
+      : this.state.date.toLocaleTimeString();
     return (
       <div>
-        <h1>{this.state.isPrecise
-          ? this.state.date.toISOString()
-          : this.state.date.toLocaleTimeString()}</h1>
-          <button onClick={this.setStateClock}>Toggle precise mode</button>
+        <h1>{this.state.clock === true ? ClockPrecise : ""}</h1>
+        <button onClick={this.setStateClock}>Toggle precise mode</button>
+        <button onClick={this.setStateClockInit}>{Show}Clock</button>
       </div>
     );
   }
@@ -36,5 +47,5 @@ export default class StateDidUpdate extends React.Component {
   componentDidMount() {
     this.startInterval();
   }
-  
+  componentWillUnmount() {}
 }
