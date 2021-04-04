003d3d9882ebd82a681bc52011c97a4b91b99683
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-04 13:34:38 -0300

    contact info component

--- study-react/src/components/LoginContact.jsx
+++ study-react/src/components/LoginContact.jsx
@@ -1,40 +1,54 @@
-import React from "react";
-export default class LoginContact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      password: "123456",
-      check: false,
-    };
-    this.check = this.check.bind(this);
+import React from 'react'
+export default class LoginContact extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      password:'123456',
+      check: false
+    }
+    this.check = this.check.bind(this)
+    this.newButtonCancel = this.newButtonCancel.bind(this)
   }
-  check(e) {
-    const valuePass = e.target.querySelector('input[type="password"]').value;
-    const checkPass = valuePass === this.state.password;
+
+  check(e){
+    const inputValue = e.target.querySelector('input[type="password"]').value;
+    const newCheck = inputValue === this.state.password;
     this.setState({
-      check: checkPass,
-    });
+      check: newCheck,
+    })
+
   }
-  render() {
+  newButtonCancel(){
+    const button = this.state.check === true ? false : true;
+    this.setState({
+      check: button
+    })
+  }
+
+
+  render(){
+
     const form = (
       <form action="#" onSubmit={this.check}>
-          <h1>Digite a senha para ver o contato</h1>
-        <input type="password" />
-        <input type="submit"  value="login" />
+        <h1>Digite a senha</h1>
+        <input type="password"/><br/>
+        <button type="submit">Abrir</button>
       </form>
     );
-
-    const contactInfo = (
-      <div>
-        <h1>Contato</h1>
-        <h2>(31)98953-7539</h2>
-      </div>
+    const contact=(
+    <div>
+      <h1>Contato</h1>
+      <h2>(31)98953-7539</h2>
+    </div>
     );
+    const buttonBack = <button onClick={this.newButtonCancel}>Fechar</button>
+
     return (
       <div>
-        {this.state.check && contactInfo}
         {!this.state.check && form}
+        {this.state.check && contact}
+        {this.state.check && buttonBack}
       </div>
-    );
+    )
   }
 }
