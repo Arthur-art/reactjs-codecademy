e1397dd33f035e2dd3cf9dd9c390e6e5ee06afd9
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-06 15:04:07 -0300

    updating QuizNavBar

--- study-react/src/components/LearningHooks/QuizNavBar.jsx
+++ study-react/src/components/LearningHooks/QuizNavBar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
-export const QuizNavaBar=()=>{
-
+export const QuizNavaBar=({questions})=>{
+/*
    const [num, setNum] = useState(0);
    const increment = ()=> setNum(num => num+=1)
    
@@ -12,4 +12,21 @@ export const QuizNavaBar=()=>{
             <button onClick={increment}>Incrementar</button>
         </div>
     )
+    */
+
+        const [firstQuestion, setQuestion] = useState(0);
+        const goNext = () => setQuestion((question)=> question +1)
+        const goBack = () => setQuestion((question)=> question -1)
+        const checkMin = firstQuestion === 0 ? true : false;
+        const checkMax = firstQuestion === 10 ? true : false;
+        
+
+        return (
+            <div>
+                <h1>Question#{firstQuestion}</h1>
+                <button onClick={goNext} disabled={checkMax} >Next</button>
+                <button onClick={goBack} disabled={checkMin}>Back</button>
+            </div>
+        )
+
 }
