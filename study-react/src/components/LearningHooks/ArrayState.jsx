c6809622bc0dd5f316d9049b1e4e318782af58f6
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-07 14:55:04 -0300

    updating Array States

--- study-react/src/components/LearningHooks/ArrayState.jsx
+++ study-react/src/components/LearningHooks/ArrayState.jsx
@@ -1,11 +1,34 @@
 import React, { useState } from 'react'
 
+const options = ["Bell Pepper", "Sausage", "Pepperoni", "Pineapple"];
+
 export const ArrayState = ()=>{
 
+    const [selected, setSelected] = useState([]);
+
+    const toggleTopping = ({target})=>{
+        const clickedTopping = target.value;
+
+        setSelected((prev)=>{
+            if(prev.includes(clickedTopping)){
+                // filtra a cobertura clicada para fora do estado
+                return prev.filter((value)=> value !== clickedTopping)
+            }else{
+                return [clickedTopping, ...prev]
+            }
+        })
+    }
+console.log(selected)
 
     return (
         <div>
-            
+            {options.map((value)=>(
+                    <button key={value} value={value} onClick={toggleTopping}>
+                    {selected.includes(value) ? 'Delete ' : 'Add '}
+                    {value}
+                </button>
+            ))}
+            <p>- Pizza, {selected.join(", ")}</p>
         </div>
     )
 }
