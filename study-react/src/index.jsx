3ce9d63cf981cf03576970c4826264133c2d0ae3
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-08 19:10:03 -0300

    updating useEffect - HooksRules study

--- study-react/src/index.jsx
+++ study-react/src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Forecas from './components/Exercises/UseEffectFetchData/FetchData'
+
 ReactDOM.render(
     <React.StrictMode>
-        <Forecas />
+       
     </React.StrictMode>,
     document.getElementById('app')
 );
