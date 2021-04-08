51e6fc7392ef20f5e72503fd76ec4bfd16655670
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-07 22:40:04 -0300

    finishing useState, starting useEffect study

--- study-react/src/index.jsx
+++ study-react/src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {AppFunction} from './components/Exercises/ConvertClassToFunctionHooks/ConvertClassToFunction'
 
 ReactDOM.render(
     <React.StrictMode>
-       <AppFunction/>
+     
     </React.StrictMode>,
     document.getElementById('app')
 );
