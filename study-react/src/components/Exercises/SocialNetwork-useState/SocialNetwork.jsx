52960870efb1fd81bf675167011ffb67d45368d8
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-11 19:57:47 -0300

    updating SocialNetWork - useEffect

--- study-react/src/components/Exercises/SocialNetwork-useState/SocialNetwork.jsx
+++ study-react/src/components/Exercises/SocialNetwork-useState/SocialNetwork.jsx
@@ -2,34 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { get } from './mockBackend/fetch';
 
 export default function SocialNetwork() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    Promise.all([get('/menu'), get('/news-feed'), get('/friends')]).then(
-      ([menuResponse, newsFeedResponse, friendsResponse]) => {
-        setData({
-          menu: menuResponse.data,
-          newsFeed: newsFeedResponse.data,
-          friends: friendsResponse.data
-        });
-      }
-    );
-  }, []);
-
+  const [menu, setMenu] = useState(null)
+  useEffect(()=>{
+    get('/menu').then((menuResponse)=>{
+      setMenu(menuResponse.data)
+    })
+  }, [])
+  const [newsFeed, setNewsFeed] = useState(null)
+  useEffect(()=>{
+    get('/news-feed').then((newsFeedResponse)=>{
+      setNewsFeed(newsFeedResponse.data)
+    })
+  }, [])
+  const [friends, setFriends] = useState(null)
+  useEffect(()=>{
+    get('/friends').then((newFriendsResponse)=>{
+      setFriends(newFriendsResponse.data)
+    })
+  }, [])
   return (
     <div className='App'>
       <h1>My Network</h1>
-      {!data || !data.menu ? <p>Loading..</p> : (
+      {!menu ? <p>Loading..</p> : (
         <nav>
-          {data.menu.map((menuItem) => (
+          {menu.map((menuItem) => (
             <button key={menuItem}>{menuItem}</button>
           ))}
         </nav>
       )}
       <div className='content'>
-        {!data || !data.newsFeed ? <p>Loading..</p> : (
+        {!newsFeed ? <p>Loading..</p> : (
           <section>
-            {data.newsFeed.map(({ id, title, message, imgSrc }) => (
+            {newsFeed.map(({ id, title, message, imgSrc }) => (
               <article key={id}>
                 <h3>{title}</h3>
                 <p>{message}</p>
@@ -38,10 +42,10 @@ export default function SocialNetwork() {
             ))}
           </section>
         )}
-        {!data || !data.friends ? <p>Loading..</p> : (
+        {!friends ? <p>Loading..</p> : (
           <aside>
             <ul>
-              {data.friends
+              {friends
                 .sort((a, b) => (a.isOnline && !b.isOnline ? -1 : 0))
                 .map(({ id, name, isOnline }) => (
                   <li key={id} className={isOnline ? 'online' : 'offline'}>
