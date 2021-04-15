6165d455ba946668c55eb936906f530b71c747d4
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-15 17:17:28 -0300

    updating - study to do list react

--- study-react/src/components/Exercises/ToDoList/Tarefas.jsx
+++ study-react/src/components/Exercises/ToDoList/Tarefas.jsx
@@ -1,59 +1,58 @@
-import React, {useState,Fragment} from 'react'
+import React, { useState, Fragment } from "react";
 
-const task = [{task:'task1'},{task:'task2'},{task:'task3'}]
+const task = [{ task: "task1" }, { task: "task2" }, { task: "task3" }];
 
-export const ToDoList = ()=>{
+export const ToDoList = () => {
 
-    const [newTask, setNewTask] = useState({});
+    const [newTask, setNewTask] = useState({})
     const handleChange = ({target})=>{
         const {name, value} = target;
         setNewTask((prev)=>({
             ...prev,
             id:Date.now(),
-            [name]: value
+            [name]:value
         }))
     }
-    
-    const [allTasks, setAllTasks]=useState([]);
+
+    const [allTasks, setAllTasks] = useState([])
     const handleSubmit = (event)=>{
-        event.preventDefault();
+        event.preventDefault()
         setAllTasks((prev)=>([
-            ...prev,
-            newTask
+            newTask,
+            ...prev
         ]))
         setNewTask({})
     }
 
-    const handleDelete=(taskId)=>{
-        setAllTasks((prev)=>prev.filter((tasks)=>{
-            return tasks.id !== taskId;
+    const handleDelete = (taskId)=>{
+        setAllTasks((prev)=>prev.filter((task)=>{
+            return task.id !== taskId;
         }))
     }
 
-    const form = (
-        <form action="#" onSubmit={handleSubmit} >
-            <input type="text" onChange={handleChange} value={newTask.task || ""}  name="task"/>
-            <button type="submit" >Adicionar</button>
-        </form>
-    )
-    const showTasks = (
-        <Fragment>
-            {allTasks.map(({task,id})=>{
-               return (
-               <Fragment>    
-               <h2 key={id}>{task}</h2>
-               <button onClick={()=>handleDelete(id)} >Delete</button>
-               </Fragment>
-               )
-            })}
-        </Fragment>
-    )
+  const form = (
+    <form action="#" onSubmit={handleSubmit}>
+      <input type="text" onChange={handleChange} value={newTask.task || ""} name="task" />
+      <button type="submit">Adicionar</button>
+    </form>
+  );
+  const showData = (
+    <ul>
+      {allTasks.map(({ task,id }) => {
+        return (
+          <li>
+            <h3 key={id}>{task}</h3>
+            <button onClick={()=>handleDelete(id)}>X</button>
+          </li>
+        );
+      })}
+    </ul>
+  );
 
-
-    return (
-        <Fragment>
-            {form}
-            {showTasks}
-        </Fragment>
-    )
-}
+  return (
+    <Fragment>
+      {form}
+      {showData}
+    </Fragment>
+  );
+};
